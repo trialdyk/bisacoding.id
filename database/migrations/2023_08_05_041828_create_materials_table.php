7ffee4504d813526c98afd112a0144e951b5d16d
Tri Aldy Kurniawan <122498697+Kenji2005@users.noreply.github.com>
2023-08-05 11:54:11 +0700

    Migration And Relation

--- database/migrations/2023_08_05_041828_create_materials_table.php
+++ database/migrations/2023_08_05_041828_create_materials_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('materials', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained();
+            $table->string('name', 100);
+            $table->text('description');
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('materials');
     }
 };
